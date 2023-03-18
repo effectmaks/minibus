13023a1879a245019d04402396077841db8715b8
Maksim Balonin <effectmaks@gmail.com>
2023-03-18 21:04:14 +0300

    Added check of have logfile

--- script/parsing/log.py
+++ script/parsing/log.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import logging
 from logging.handlers import TimedRotatingFileHandler
@@ -8,6 +9,10 @@ level = logging.DEBUG
 if platform.system().startswith('L'):
     FILE_PATH = 'logs/py_log.log'  # если запускать с Linux python3 start.py
     level = logging.INFO
+
+# Проверяем наличие файла по пути FILE_PATH и создаем его при отсутствии
+if not os.path.exists(FILE_PATH):
+    open(FILE_PATH, 'a').close()
 
 log_format = "%(asctime)s-%(levelname)s: %(message)s"
 stream_handler = logging.StreamHandler()
@@ -21,37 +26,3 @@ logging.basicConfig(level=level, format=log_format,
                     )
 
 logger = logging.getLogger(FILE_PATH)
-
-"""
-
-
-FILE_PATH = '../logs/py_log_debug'
-level = logging.DEBUG
-if platform.system().startswith('L'):
-    FILE_PATH = 'logs/py_log'  # если запускать с Linux python3 start.py
-    level = logging.INFO
-
-log_format = "%(asctime)s-%(levelname)s: %(message)s"
-logger = logging.getLogger(FILE_PATH)
-logger.setLevel(level)
-
-# настройка обработчика и форматировщика для logger2
-handler_file = logging.FileHandler(f"{FILE_PATH}.log", mode='a')
-formatter = logging.Formatter(log_format)
-
-# добавление форматировщика к обработчику
-handler_file.setFormatter(formatter)
-# добавление обработчика к логгеру
-logger.addHandler(handler_file)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-
-logging.basicConfig(level=level, format=log_format,
-                    handlers=[handler_file,
-                              stream_handler],
-
-                    )
-
-logger.info(f"Testing logger {FILE_PATH}...")
-"""
